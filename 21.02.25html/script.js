76a3c7545b9587cea57bd7d4566a0658ec3d00fb
paulapujate <paulapujate@gmail.com>
2025-02-21 12:47:39 +0200

    ';LP

---
+++ 21.02.25html/script.js
@@ -0,0 +1,172 @@
+alert("Hello");
+let month = "Janvaris";
+switch(month.toLowerCase()){
+
+    case "Decembris":
+        alert("Ziema");
+    break;
+
+    case "Janvāris":
+        alert("Ziema");
+    break;
+
+    case "Februāris":
+        alert("Ziema");
+    break;
+
+    case "Marts":
+        alert("Pavasaris");
+    break;
+
+    case "Aprilis":
+        alert("Pavasaris");
+    break;  
+
+    case "Maijs":
+        alert("Pavasaris");
+    break;
+
+    case "Jūnijs":
+        alert("Vasara");
+    break;  
+
+    case "Julijs":
+        alert("Vasara");
+    break;  
+
+    
+    case "Augusts":
+        alert("Vasara");
+    break;  
+
+    case "Septembris":
+        alert("Rudens");
+    break;  
+
+    case "Oktobris":
+        alert("Rudens");
+    break;  
+
+    case "Novembris":
+        alert("Rudens");
+    break;  
+}
+
+let diena = 5;
+switch(diena) {
+
+
+case 1:
+alert("darbadiena");
+break;
+
+case 2:
+alert("darbadiena");
+break;
+
+case 3:
+ alert("darbadiena");
+break;
+
+case 4:
+alert("darbadiena");
+break;
+
+case 5:
+ alert("darbadiena");
+break;
+
+case 6:
+ alert("brīvdiena");
+break;
+
+case 7:
+ alert("brīvdiena")
+ ;
+break;
+
+}
+let Atzime = 10;
+switch(Atzime){
+
+    case 10:
+        alert("Labi");
+       break;
+
+       case 9:
+        alert("Labi");
+       break;
+
+    case 8:
+        alert("Labi");
+       break;
+
+
+       case 7:
+        alert("Vidēji");
+       break;
+
+       case 6:
+        alert("Vidēji");
+       break;
+
+       case 5:
+        alert("Vidēji");
+       break;
+
+
+       case 4:
+        alert("Slikti");
+       break;
+
+       case 3:
+        alert("Slikti");
+       break;
+
+       case 2:
+        alert("Slikti");
+       break;
+
+       case 1:
+        alert("Slikti");
+       break;
+
+}
+let valuta = "EUR";
+switch(valuta){
+
+    case "EUR":
+        alert("Eiro");
+       break;
+
+       case "GBP":
+        alert("Lielbritānijas mārciņa");
+       break;
+
+       case "USD":
+        alert("Amerikas dolārs");
+       break;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
