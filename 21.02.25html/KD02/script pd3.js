5c1a9c061df2ae760d699f8555633529aa457996
paulapujate <paulapujate@gmail.com>
2025-04-25 14:58:48 +0300

    labi

---
+++ 21.02.25html/KD02/script pd3.js
@@ -0,0 +1,21 @@
+console.log ("vards:", vards);
+console.log("vecums:",vecums);
+comsole.log("pilsonība:" pilsoiba);
+ if (vecums>=18 && irpilsonis) {
+    console.log ("drīkst balsot.");
+ } else if (vecums >= 18 &&!irpilsonis) {
+    console.log('personai ir ${vecums} gadi, bet vina nav pilsonis.');
+ } else if (vecums < 18 && irPilsonis) {
+
+ }console.log ("Persona nav pilngadīga, bet ir pilsonis.");{
+
+ } else {
+    console.log("prsona nav pilngadiga un nav pilsonis.");
+
+ }
+ 
+  
+
+    
+
+
